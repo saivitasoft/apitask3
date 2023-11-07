115eaec55b0372a08777e62241c7a3f9041815e1
saivitasoft <saikumarvita@gmail.com>
2023-11-07 11:49:53 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsideComponent } from './aside/aside.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    NavComponent,
 
   ],
   imports: [
