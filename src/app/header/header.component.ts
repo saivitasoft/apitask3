e82499db51b5a8b95a13578297a125768c733c89
saivitasoft <saikumarvita@gmail.com>
2023-11-08 12:01:52 +0530

    footer to section copy data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SaiapiService } from '../saiapi.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent  implements OnInit{
+  apiData! :any;
+ constructor(private apiService: SaiapiService){}
 
+ ngOnInit(): void {
+
+   this.apiService.getData().subscribe((res: any) => {
+    
+     console.log(res);
+     this.apiData = res
+     console.log(this.apiData);
+
+
+   })
+  }
 }
--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SaiapiService } from '../saiapi.service';
+
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.css']
 })
-export class SectionComponent {
+export class SectionComponent  implements OnInit{
+  apiData! :any;
+ constructor(private apiService: SaiapiService){}
 
+ ngOnInit(): void {
+
+   this.apiService.getData().subscribe((res: any) => {
+    
+     console.log(res);
+     this.apiData = res
+     console.log(this.apiData);
+
+
+   })
+  }
 }
