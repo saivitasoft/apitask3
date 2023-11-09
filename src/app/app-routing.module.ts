ce3553cd7ed0dbd0f4e744654885b3b65399b95e
saivitasoft <saikumarvita@gmail.com>
2023-11-09 12:29:24 +0530

    header fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AsideComponent } from './aside/aside.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { SectionComponent } from './section/section.component';
 
 
 const routes: Routes = [
   {path:'aside',component:AsideComponent},
   {path:'footer',component:FooterComponent},
   {path:'header',component:HeaderComponent},
-  {path:'nav',component:NavComponent}
+  {path:'nav',component:NavComponent},
+  {path:'section',component:SectionComponent}
  
 ];
 
